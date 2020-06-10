3a35246807abb0ddb761c93e58dfcc0cafa71495
GaiusPetronius <TheGaiusPetronius@gmail.com>
2020-06-10 08:36:15 +0200

    Improved movement to be smooth, adding speed changing, bigger screen

--- game.py
+++ game.py
@@ -1,16 +1,17 @@
 import pygame as pg
 
+# Test
 pg.init()
-screen_width = 400
-screen_height = 300
+screen_width = 800
+screen_height = 600
 screen = pg.display.set_mode((screen_width, screen_height))
 finished = False
 
 square_is_teal = True
 teal_color = (42, 222, 222)
 red_color = (222, 42, 42)
-x_pos = 175
-y_pos = 125
+x_pos = 375
+y_pos = 275
 movement_speed = 4
 clock = pg.time.Clock()
 
@@ -18,21 +19,34 @@ clock = pg.time.Clock()
 while not finished:
     # Process events
     movement_delta = {"x": 0, "y": 0}
+    speed_delta = 0
     for event in pg.event.get():
         if event.type == pg.QUIT:
             finished = True
-        if event.type == pg.KEYDOWN:
-            square_is_teal = not square_is_teal
+        elif event.type == pg.KEYDOWN:
+            if event.key == pg.K_f:
+                square_is_teal = not square_is_teal
+            if event.key == pg.K_KP_PLUS:
+                speed_delta += 1
+            if event.key == pg.K_KP_MINUS:
+                speed_delta -= 1
 
-        pressed_keys = pg.key.get_pressed()
-        if pressed_keys[pg.K_UP]:
-            movement_delta['y'] -= movement_speed
-        if pressed_keys[pg.K_DOWN]:
-            movement_delta['y'] += movement_speed
-        if pressed_keys[pg.K_LEFT]:
-            movement_delta['x'] -= movement_speed
-        if pressed_keys[pg.K_RIGHT]:
-            movement_delta['x'] += movement_speed
+    # Update speed
+    if speed_delta != 0:
+        movement_speed += speed_delta
+        if movement_speed < 0:
+            movement_speed = 0
+
+    # Get position input
+    pressed_keys = pg.key.get_pressed()
+    if pressed_keys[pg.K_UP]:
+        movement_delta['y'] -= movement_speed
+    if pressed_keys[pg.K_DOWN]:
+        movement_delta['y'] += movement_speed
+    if pressed_keys[pg.K_LEFT]:
+        movement_delta['x'] -= movement_speed
+    if pressed_keys[pg.K_RIGHT]:
+        movement_delta['x'] += movement_speed
 
     # Update position
     updated_x_pos = x_pos + movement_delta['x']
